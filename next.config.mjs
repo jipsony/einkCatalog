5f7da491502a3e8bfa1d6fc96bf86700e020b2ab
jipsony <106913652+jipsony@users.noreply.github.com>
2026-06-02 23:15:55 +0200

    to next 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
   experimental: {
+    reactCompiler: true,
     optimizePackageImports: ["@chakra-ui/react"],
   },
 };
